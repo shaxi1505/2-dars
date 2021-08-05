314ee5ca3f5bfe02513617046f38d8a88b849851
shaxi1505 <kamolovashohzoda@gmail.com>
2021-08-05 12:34:30 +0500

    yaratish va o'rganish

--- config/settings.py
+++ config/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-mo#(%g1!n)@4*jw=wy0r-ez=em!%@76%o7psznl91&@ha$gmvd
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
